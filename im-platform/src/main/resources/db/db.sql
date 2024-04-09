2e8b6c3bab3b8c1abc774c89de8afc5ca42c789d
jianguo <jianguo5187@gmail.com>
2024-04-09 21:06:10 +0800

    0409

--- im-platform/src/main/resources/db/db.sql
+++ im-platform/src/main/resources/db/db.sql
@@ -1,81 +1,162 @@
-use `box-im`;
-create table `im_user`(
-    `id` bigint not null auto_increment primary key  comment 'id',
-    `user_name` varchar(255) not null comment '用户名',
-    `nick_name` varchar(255) not null comment '用户昵称',
-    `head_image` varchar(255) default '' comment '用户头像',
-    `head_image_thumb` varchar(255) default '' comment '用户头像缩略图',
-    `password` varchar(255) not null comment '密码(明文)',
-    `sex`  tinyint(1) default 0 comment '性别 0:男 1:女',
-    `type`  smallint default 1 comment '用户类型 1:普通用户 2:审核账户',
-    `signature` varchar(1024) default '' comment '个性签名',
-    `last_login_time`  datetime DEFAULT null comment '最后登录时间',
-    `created_time` datetime DEFAULT CURRENT_TIMESTAMP comment '创建时间',
-    unique key `idx_user_name`(user_name),
-    key `idx_nick_name`(nick_name)
-) ENGINE=InnoDB CHARSET=utf8mb3 comment '用户';
+/*
+ Navicat Premium Data Transfer
 
-create table `im_friend`(
-    `id` bigint not null auto_increment primary key  comment 'id',
-    `user_id` bigint not null  comment '用户id',
-    `friend_id` bigint not null  comment '好友id',
-    `friend_nick_name` varchar(255) not null comment '好友昵称',
-    `friend_head_image` varchar(255) default '' comment '好友头像',
-    `created_time` datetime DEFAULT CURRENT_TIMESTAMP comment '创建时间',
-    key `idx_user_id` (`user_id`),
-    key `idx_friend_id` (`friend_id`)
-) ENGINE=InnoDB CHARSET=utf8mb3 comment '好友';
+ Source Server         : boxim
+ Source Server Type    : MySQL
+ Source Server Version : 50726 (5.7.26)
+ Source Host           : localhost:3306
+ Source Schema         : boxim
 
-create table `im_private_message`(
-    `id` bigint not null auto_increment primary key comment 'id',
-    `send_id` bigint not null  comment '发送用户id',
-    `recv_id` bigint not null  comment '接收用户id',
-    `content` text   comment '发送内容',
-    `type`  tinyint(1) NOT NULL  comment '消息类型 0:文字 1:图片 2:文件 3:语音 10:系统提示',
-    `status` tinyint(1) NOT NULL   comment '状态 0:未读 1:已读 2:撤回',
-    `send_time` datetime DEFAULT CURRENT_TIMESTAMP comment '发送时间',
-    key `idx_send_recv_id` (`send_id`,`recv_id`)
-)ENGINE=InnoDB CHARSET=utf8mb3 comment '私聊消息';
+ Target Server Type    : MySQL
+ Target Server Version : 50726 (5.7.26)
+ File Encoding         : 65001
 
+ Date: 09/04/2024 21:05:58
+*/
 
-create table `im_group`(
-    `id` bigint not null auto_increment primary key comment 'id',
-    `name` varchar(255) not null comment '群名字',
-    `owner_id` bigint not null  comment '群主id',
-    `head_image` varchar(255) default '' comment '群头像',
-    `head_image_thumb` varchar(255) default '' comment '群头像缩略图',
-    `notice` varchar(1024)  default '' comment '群公告',
-    `remark` varchar(255) default '' comment '群备注',
-    `deleted` tinyint(1) default 0   comment '是否已删除',
-    `created_time` datetime default CURRENT_TIMESTAMP comment '创建时间'
-)ENGINE=InnoDB CHARSET=utf8mb3 comment '群';
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-create table `im_group_member`(
-    `id` bigint not null auto_increment primary key comment 'id',
-    `group_id` bigint not null  comment '群id',
-    `user_id` bigint not null  comment '用户id',
-    `alias_name` varchar(255) DEFAULT '' comment '组内显示名称',
-    `head_image` varchar(255) DEFAULT '' comment '用户头像',
-    `remark` varchar(255) DEFAULT '' comment '备注',
-    `quit` tinyint(1) DEFAULT 0  comment '是否已退出',
-    `quit_time` datetime DEFAULT NULL comment '退出时间',
-    `created_time` datetime DEFAULT CURRENT_TIMESTAMP comment '创建时间',
-    key `idx_group_id`(`group_id`),
-    key `idx_user_id`(`user_id`)
-)ENGINE=InnoDB CHARSET=utf8mb3 comment '群成员';
+-- ----------------------------
+-- Table structure for im_friend
+-- ----------------------------
+DROP TABLE IF EXISTS `im_friend`;
+CREATE TABLE `im_friend`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `friend_id` bigint(20) NOT NULL COMMENT '好友id',
+  `friend_nick_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '好友昵称',
+  `friend_head_image` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '好友头像',
+  `created_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_user_id`(`user_id`) USING BTREE,
+  INDEX `idx_friend_id`(`friend_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '好友' ROW_FORMAT = Dynamic;
 
-create table `im_group_message`(
-    `id` bigint not null auto_increment primary key comment 'id',
-    `group_id` bigint not null  comment '群id',
-    `send_id` bigint not null  comment '发送用户id',
-    `send_nick_name` varchar(255) DEFAULT ''  comment '发送用户昵称',
-    `recv_ids` varchar(1024) DEFAULT ''  comment '接收用户id,逗号分隔，为空表示发给所有成员',
-    `content` text   comment '发送内容',
-    `at_user_ids` varchar(1024) comment '被@的用户id列表，逗号分隔',
-    `receipt` tinyint DEFAULT 0  comment '是否回执消息',
-    `receipt_ok` tinyint DEFAULT 0  comment '回执消息是否完成',
-    `type`  tinyint(1) NOT NULL  comment '消息类型 0:文字 1:图片 2:文件 3:语音 4:视频 10:系统提示' ,
-    `status` tinyint(1) DEFAULT 0 comment '状态 0:未发出 1:已送达  2:撤回 3:已读',
-    `send_time` datetime DEFAULT CURRENT_TIMESTAMP comment '发送时间',
-    key `idx_group_id` (group_id)
-)ENGINE=InnoDB CHARSET=utf8mb3 comment '群消息';
+-- ----------------------------
+-- Records of im_friend
+-- ----------------------------
+INSERT INTO `im_friend` VALUES (1, 2, 1, '测试1', '', '2024-04-09 20:37:33');
+INSERT INTO `im_friend` VALUES (2, 1, 2, '测试2', '', '2024-04-09 20:37:33');
+
+-- ----------------------------
+-- Table structure for im_group
+-- ----------------------------
+DROP TABLE IF EXISTS `im_group`;
+CREATE TABLE `im_group`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '群名字',
+  `owner_id` bigint(20) NOT NULL COMMENT '群主id',
+  `head_image` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '群头像',
+  `head_image_thumb` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '群头像缩略图',
+  `notice` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '群公告',
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '群备注',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否已删除',
+  `created_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '群' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of im_group
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for im_group_member
+-- ----------------------------
+DROP TABLE IF EXISTS `im_group_member`;
+CREATE TABLE `im_group_member`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `group_id` bigint(20) NOT NULL COMMENT '群id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `alias_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '组内显示名称',
+  `head_image` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '用户头像',
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '备注',
+  `quit` tinyint(1) NULL DEFAULT 0 COMMENT '是否已退出',
+  `quit_time` datetime NULL DEFAULT NULL COMMENT '退出时间',
+  `created_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_group_id`(`group_id`) USING BTREE,
+  INDEX `idx_user_id`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '群成员' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of im_group_member
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for im_group_message
+-- ----------------------------
+DROP TABLE IF EXISTS `im_group_message`;
+CREATE TABLE `im_group_message`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `group_id` bigint(20) NOT NULL COMMENT '群id',
+  `send_id` bigint(20) NOT NULL COMMENT '发送用户id',
+  `send_nick_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '发送用户昵称',
+  `recv_ids` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '接收用户id,逗号分隔，为空表示发给所有成员',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '发送内容',
+  `at_user_ids` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '被@的用户id列表，逗号分隔',
+  `receipt` tinyint(4) NULL DEFAULT 0 COMMENT '是否回执消息',
+  `receipt_ok` tinyint(4) NULL DEFAULT 0 COMMENT '回执消息是否完成',
+  `type` tinyint(1) NOT NULL COMMENT '消息类型 0:文字 1:图片 2:文件 3:语音 4:视频 10:系统提示',
+  `status` tinyint(1) NULL DEFAULT 0 COMMENT '状态 0:未发出 1:已送达  2:撤回 3:已读',
+  `send_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发送时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_group_id`(`group_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '群消息' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of im_group_message
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for im_private_message
+-- ----------------------------
+DROP TABLE IF EXISTS `im_private_message`;
+CREATE TABLE `im_private_message`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `send_id` bigint(20) NOT NULL COMMENT '发送用户id',
+  `recv_id` bigint(20) NOT NULL COMMENT '接收用户id',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '发送内容',
+  `type` tinyint(1) NOT NULL COMMENT '消息类型 0:文字 1:图片 2:文件 3:语音 10:系统提示',
+  `status` tinyint(1) NOT NULL COMMENT '状态 0:未读 1:已读 2:撤回',
+  `send_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发送时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_send_recv_id`(`send_id`, `recv_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '私聊消息' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of im_private_message
+-- ----------------------------
+INSERT INTO `im_private_message` VALUES (1, 2, 1, '123', 0, 3, '2024-04-09 20:37:44');
+INSERT INTO `im_private_message` VALUES (2, 1, 2, '3333', 0, 3, '2024-04-09 20:37:51');
+INSERT INTO `im_private_message` VALUES (3, 1, 2, '{\"originUrl\":\"http://127.0.0.1:9001/box-im/image/20240409/1712666326874.jpeg\",\"thumbUrl\":\"http://127.0.0.1:9001/box-im/image/20240409/1712666326874.jpeg\"}', 1, 3, '2024-04-09 20:38:47');
+INSERT INTO `im_private_message` VALUES (4, 1, 2, '{\"originUrl\":\"http://127.0.0.1:9001/box-im/image/20240409/1712666358399.jpeg\",\"thumbUrl\":\"http://127.0.0.1:9001/box-im/image/20240409/1712666358399.jpeg\"}', 1, 3, '2024-04-09 20:39:19');
+
+-- ----------------------------
+-- Table structure for im_user
+-- ----------------------------
+DROP TABLE IF EXISTS `im_user`;
+CREATE TABLE `im_user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `nick_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户昵称',
+  `head_image` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '用户头像',
+  `head_image_thumb` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '用户头像缩略图',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码(明文)',
+  `sex` tinyint(1) NULL DEFAULT 0 COMMENT '性别 0:男 1:女',
+  `type` smallint(6) NULL DEFAULT 1 COMMENT '用户类型 1:普通用户 2:审核账户',
+  `signature` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '个性签名',
+  `last_login_time` datetime NULL DEFAULT NULL COMMENT '最后登录时间',
+  `created_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_user_name`(`user_name`) USING BTREE,
+  INDEX `idx_nick_name`(`nick_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of im_user
+-- ----------------------------
+INSERT INTO `im_user` VALUES (1, 'lxh1', '测试1', '', '', '$2a$10$z2dG2VGv728.g7C2sPtlg.0S1qsgYAOxtTbVRd.7y7bVq2aWma8a6', 0, 1, '', NULL, '2024-04-09 20:18:54');
+INSERT INTO `im_user` VALUES (2, 'lxh2', '测试2', '', '', '$2a$10$Zx.3MGTvRiwF/SVSPaNc1u91rpFpNaCl2xau5h.gOcH7H5jrH74Py', 0, 1, '', NULL, '2024-04-09 20:37:07');
+
+SET FOREIGN_KEY_CHECKS = 1;
