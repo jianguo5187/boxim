3c3691c72f721977fedbafccef2abfd19df4e72a
jianguo <jianguo5187@gmail.com>
2024-08-05 22:59:33 +0800

    0805

--- im-uniapp/.env.js
+++ im-uniapp/.env.js
@@ -1,7 +1,7 @@
 //设置环境(打包前修改此变量)
 const ENV = "PROD";
 const UNI_APP = {}
-if(ENV=="PROD"){
+if(ENV=="DEV"){
 	UNI_APP.BASE_URL = "http://127.0.0.1:8888";
 	UNI_APP.WS_URL = "ws://127.0.0.1:8878/im";
 	// H5 走本地代理解决跨域问题
@@ -10,7 +10,15 @@ if(ENV=="PROD"){
 	// #endif
 }
 if(ENV=="PROD"){
-	UNI_APP.BASE_URL = "http://43.129.82.76:81/api";
-	UNI_APP.WS_URL = "ws://43.129.82.76:8878/im";
+	// UNI_APP.BASE_URL = "http://43.159.197.22:82/api";
+	// UNI_APP.WS_URL = "ws://43.159.197.22:8878/im";
+	// H5 走本地代理解决跨域问题
+	// #ifdef H5
+		UNI_APP.BASE_URL = "/api";
+	// #endif
+	// UNI_APP.BASE_URL = "http://43.129.82.76:81/api";
+	// UNI_APP.WS_URL = "ws://43.129.82.76:8878/im";
+	// UNI_APP.BASE_URL = "http://43.132.168.72:82/api";
+	UNI_APP.WS_URL = "ws://43.132.168.72:8878/im";
 }
 export default UNI_APP
