086f737d5cd5b9a0958c7f3752a34870a991daaf
jianguo <jianguo5187@gmail.com>
2024-10-23 22:12:32 +0800

    1023

--- im-uniapp/.env.js
+++ im-uniapp/.env.js
@@ -14,12 +14,14 @@ if(ENV=="PROD"){
 	// UNI_APP.WS_URL = "ws://43.159.197.22:8878/im";
 	// H5 走本地代理解决跨域问题
 	// #ifdef H5
-		UNI_APP.BASE_URL = "/api";
+		// UNI_APP.BASE_URL = "/api";
 	// #endif
 	// UNI_APP.BASE_URL = "http://43.129.82.76:81/api";
 	// UNI_APP.WS_URL = "ws://43.129.82.76:8878/im";
 	// UNI_APP.BASE_URL = "http://43.132.168.72:82/api";
 	// UNI_APP.WS_URL = "ws://43.132.168.72:8878/im";
-	UNI_APP.WS_URL = "ws://43.132.182.230:8878/im";
+	// UNI_APP.WS_URL = "ws://47.57.188.97:8878/im";
+	UNI_APP.BASE_URL = "https://ngbt.qdcspy.shop/api";
+	UNI_APP.WS_URL = "wss://ngbt.qdcspy.shop/im";
 }
 export default UNI_APP
